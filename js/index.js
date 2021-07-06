034d13e01d84231bf140a1523065ccd4ab0ef86f
ashleyannlaz <paperpastels@gmail.com>
2021-07-06 19:40:27 -0400

    Finished event listeners

--- js/index.js
+++ js/index.js
@@ -1,17 +1,96 @@
 
+// 1
 
-// 7. Blur / focus event
+// 2
+const card = document.querySelector('.intro img');
 
-const password = document.querySelector('input[type="password"]');
-
-password.addEventListener('focus', (event) => {
-  event.target.style.background = 'pink';
+card.addEventListener('dblclick', function (e) {
+    card.toggleAttribute('hidden');
 });
 
-password.addEventListener('blur', (event) => {
-  event.target.style.background = '';
-});
 
+// 3 Window size
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
+function doSomething(scrollPos) {
+  console.log(scrollPos)
+}
+
+document.addEventListener('scroll', function(e) {
+  lastKnownScrollPosition = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(lastKnownScrollPosition);
+      ticking = false;
+    } );
+
+    ticking = true;
+  }
+}, {once:true});
+
+// 4  Load Event
+window.addEventListener('load', (event) => {
+    console.log('Ready to go, page is loaded.');
+  });
+
+// 5-7. Opt In Form
+
+let signUp = document.createElement("p");
+signUp.style.marginLeft = '15px';
+signUp.style.marginTop = '15px';
+signUp.textContent = "Login to your account:";
+
+const mailForm = document.querySelector('.content-destination');
+mailForm.appendChild(signUp);
+
+signUp.addEventListener('mouseover', function(event) {
+
+
+// Create the Form
+let form = document.createElement("form");
+form.setAttribute("method","post");
+form.setAttribute("action","submit.php");
+form.style.marginTop = '15px';
+form.style.marginLeft = '15px';
+
+let ID = document.createElement("input");
+ID.setAttribute("type", "text");
+ID.setAttribute("name", "emailID");
+ID.setAttribute("placeholder", "E-Mail");
+  
+// Create an input element for password
+let PWD = document.createElement("input");
+PWD.setAttribute("type", "password");
+PWD.setAttribute("name", "password");
+PWD.setAttribute("placeholder", "Password");
+PWD.addEventListener('blur' , (event) => {
+    event.target.style.background = "";
+})
+PWD.addEventListener('focus' , (event) => {
+    event.target.style.background = "gray";
+})
+  
+// Create a submit button
+let s = document.createElement("input");
+s.setAttribute("type", "submit");
+s.setAttribute("value", "Submit");
+  
+// Append the email_ID input to the form
+form.append(ID); 
+                
+// Append the password to the form
+form.append(PWD); 
+                
+// Append the button to the form
+form.append(s); 
+
+const headerForm = document.querySelector('.content-destination');
+headerForm.appendChild(form);
+
+}, { once: true })
+               
 //-------------------------------------------------------------------------
 // 8. Buttons
 const buttons = document.querySelectorAll('.btn')
